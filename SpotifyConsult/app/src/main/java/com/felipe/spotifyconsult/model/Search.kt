9c1f1d869cf4e303aa63d47136c170c709485572
felipe.martins <felipe.martins@leucotron.com.br>
2018-12-21 14:42:48 -0200

    Searches  model.

--- SpotifyConsult/app/src/main/java/com/felipe/spotifyconsult/model/Search.kt
+++ SpotifyConsult/app/src/main/java/com/felipe/spotifyconsult/model/Search.kt
@@ -1,2 +1,20 @@
 package com.felipe.spotifyconsult.model
 
+import com.google.gson.annotations.SerializedName
+
+class Search {
+
+    @SerializedName("q")
+    private var q: String = ""
+    @SerializedName("type")
+    private var type: String = ""
+    @SerializedName("market")
+    private var market: String? = null
+    @SerializedName("limit")
+    private var limit: Int? = 20
+    @SerializedName("offset")
+    private var offset: Int? = 0
+    @SerializedName("include_external")
+    private var includeExternal: String? = null
+
+}
