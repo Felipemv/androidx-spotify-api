2cde66420bceca3d48a7318bef514fe2dc8649be
Felipemv <felipe.martinsvitor@gmail.com>
2018-12-24 19:45:46 -0200

    Splash screen.

--- SpotifyConsult/app/src/main/java/com/felipe/spotifyconsult/SpotifyConsultApplication.kt
+++ SpotifyConsult/app/src/main/java/com/felipe/spotifyconsult/SpotifyConsultApplication.kt
@@ -3,7 +3,7 @@ package com.felipe.spotifyconsult
 import android.app.Application
 import android.content.Intent
 import com.facebook.stetho.Stetho
-import com.felipe.spotifyconsult.activity.MainActivity
+import com.felipe.spotifyconsult.activity.SplashActivity
 
 class SpotifyConsultApplication : Application() {
 
@@ -11,6 +11,6 @@ class SpotifyConsultApplication : Application() {
         super.onCreate()
         Stetho.initializeWithDefaults(this)
 
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, SplashActivity::class.java))
     }
 }
---
+++ SpotifyConsult/app/src/main/java/com/felipe/spotifyconsult/activity/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.felipe.spotifyconsult.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.felipe.spotifyconsult.R
+
+class SplashActivity : AppCompatActivity(), Runnable {
+
+    var handler: Handler? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+        handler!!.postDelayed(this, 3000)
+    }
+
+    override fun onDestroy() {
+        if (handler != null) {
+            handler!!.removeCallbacks(this)
+        }
+        super.onDestroy()
+    }
+
+    override fun run() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
